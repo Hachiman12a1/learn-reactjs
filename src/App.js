cfbb06be5dcad1f3cc8af59b9dc2afacf6f9abf9
Dao Van Dat <daovandat2k15@gmail.com>
2022-07-30 16:47:25 +0700

    128. Làm chức năng Pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 // import logo from "./logo.svg";
 import Header from "components/Header";
 import ProductFeature from "features/Product";
-import { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import productApi from "./api/productApi";
 import "./App.css";
 import NotFound from "./components/NotFound";
 import AlbumFeature from "./features/Album";
@@ -11,18 +9,6 @@ import CounterFeature from "./features/Counter";
 import TodoFeature from "./features/Todo";
 
 function App() {
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const params = {
-        _limit: 10,
-      };
-      const productList = await productApi.getAll(params);
-      console.log(productList);
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
     <div className="App">
       <Header />
