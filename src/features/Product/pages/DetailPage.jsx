6dc704cfc9812cc71b70545a68135791c7644135
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-01 17:32:22 +0700

    152. Code các sub-components của product

--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Box, Container, Grid, makeStyles, Paper } from "@material-ui/core";
 import ProductThumbnail from "../components/ProductThumbnail";
-import { useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import useProductDetail from "../hooks/useProductDetail";
 import ProductInfo from "../components/ProductInfo";
 import AddToCartForm from "../components/AddToCartForm";
 import ProductMenu from "../components/ProductMenu";
+import ProductDescription from "../components/ProductDescription";
+import ProductAdditional from "../components/ProductAdditional";
+import ProductReviews from "../components/ProductReviews";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -25,18 +28,19 @@ function DetailPage() {
 
   const {
     params: { productId },
+    url,
   } = useRouteMatch();
 
   const { product, loading } = useProductDetail(productId);
 
-  if(loading) {
+  if (loading) {
     // TODO : make this beautiful
-    return <Box>Loading</Box>
+    return <Box>Loading</Box>;
   }
 
   const handleAddToCartSubmit = (formValues) => {
-    console.log('Form submit', formValues);
-  }
+    console.log("Form submit", formValues);
+  };
   return (
     <Box className={classes.root}>
       <Container>
@@ -46,13 +50,20 @@ function DetailPage() {
               <ProductThumbnail product={product} />
             </Grid>
             <Grid item className={classes.right}>
-              <ProductInfo product={product}/>
-              <AddToCartForm onSubmit = {handleAddToCartSubmit}/>
+              <ProductInfo product={product} />
+              <AddToCartForm onSubmit={handleAddToCartSubmit} />
             </Grid>
           </Grid>
         </Paper>
-        <ProductMenu/>
-        
+        <ProductMenu />
+        <Switch>
+          <Route exact path={url}>
+            <ProductDescription product={product} />
+          </Route>
+
+          <Route path={`${url}/additional`} component={ProductAdditional} />
+          <Route path={`${url}/reviews`} component={ProductReviews} />
+        </Switch>
       </Container>
     </Box>
   );
