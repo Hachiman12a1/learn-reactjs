6a0a966a2113db877b9baf421b7822af3245c8a8
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-01 17:58:13 +0700

    153. An toàn render dữ liệu HTML lên UI

--- src/features/Product/components/ProductDescription.jsx
+++ src/features/Product/components/ProductDescription.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Paper } from "@material-ui/core";
+import DOMPurify from 'dompurify';
 
-ProductDescription.propTypes = {};
 
-function ProductDescription(props) {
+ProductDescription.propTypes = {
+  product: PropTypes.object,
+};
+
+function ProductDescription({ product = {} }) {
+    const saveDescription = DOMPurify.sanitize(product.description)
   return (
-  <div>
-    Description
-  </div>);
+    <Paper elevation={0} style={{ padding: "15px" }}>
+      <div dangerouslySetInnerHTML={{ __html: saveDescription }} />
+    </Paper>
+  );
 }
 
 export default ProductDescription;
