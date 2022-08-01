e40ee2454a10fe99354d8021ce64490fccf72ae4
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-01 17:32:53 +0700

    152. Code các sub-components của product

---
+++ src/features/Product/components/ProductDescription.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+ProductDescription.propTypes = {};
+
+function ProductDescription(props) {
+  return (
+  <div>
+    Description
+  </div>);
+}
+
+export default ProductDescription;
