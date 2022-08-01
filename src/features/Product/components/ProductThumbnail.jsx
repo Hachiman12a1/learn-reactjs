d48359e824e0a8d9bfd09061433792effd959dd5
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-01 11:15:12 +0700

    145. Code component ProductThumbnail

---
+++ src/features/Product/components/ProductThumbnail.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@material-ui/core";
+import { THUMBNAIL_PLACEHOLDER } from "constants/common";
+import { STATIC_HOST } from "constants/index";
+import PropTypes from "prop-types";
+
+ProductThumbnail.propTypes = {
+  product: PropTypes.func,
+};
+
+function ProductThumbnail(props) {
+  const { product } = props;
+
+  const thumbnailUrl = product.thumbnail
+    ? `${STATIC_HOST}${product.thumbnail?.url}`
+    : THUMBNAIL_PLACEHOLDER;
+
+  return (
+    <Box>
+      <img src={thumbnailUrl} alt={product.name} width="100%" />
+    </Box>
+  );
+}
+
+export default ProductThumbnail;
